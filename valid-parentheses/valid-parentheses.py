f66c162841f8d00a86ff5a32599c23c419accaa4
Yune Zheng <13390592073@163.com>
2021-08-19 01:16:40 +0800

    valid-parentheses conclusion

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -19,11 +19,15 @@ class Solution:
           stack.append(map[x]) #what really enter in the stack is the counterpart of },],) ,
                             # because when make a comparision we need to },],)  使用括号的右半部分来做相等比较
     # 如果遇到右半边括号时，分类讨论：
-    # 1）如栈不为空且为对应的左半边括号，则取出栈顶元素，继续循环
-    # 2）若此时栈为空，则直接返回 false
-    # 3）若不为对应的左半边括号，反之返回 false
+    
+    #判断题做题步骤 1 分情况讨论 2 想哪一个情况范围大，哪一个情况范围小，其实一般人类最直观的情况都是范围小的
+    #3.范围大的情况的ifelse在外面 范围小的ifelse在里面
+    #The break statement, like in C, breaks out of the innermost enclosing for or while loop.
+    #The continue statement, also borrowed from C, continues with the next iteration of the loop:
+    #需要 两个容器 一个 dict 一个stack，其实是个list
+
         else:
-          if len(stack)!=0:
+          if len(stack)!=0:   # 1）如栈不为空且为对应的左半边括号，则取出栈顶元素，继续循环
             top_element = stack.pop()
             if x != top_element:
               return False # 3）若不为对应的左半边括号，反之返回 false
